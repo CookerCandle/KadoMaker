f474550e49db56dc4c5c78c8b2849e7d339f50c2
Jamoliddin <164184291+CookerCandle@users.noreply.github.com>
2025-09-06 21:19:04 +0500

    Modify load_font to include font name parameter

--- sources/fonts.py
+++ sources/fonts.py
@@ -3,7 +3,10 @@ import os
 from reportlab.pdfbase.ttfonts import TTFont
 from reportlab.pdfbase import pdfmetrics
 
-def load_font(font_path):
+def load_font(name: str, font_path: str):
+    """
+    Регистрирует шрифт в ReportLab под указанным именем.
+    """
     if not os.path.exists(font_path):
         raise FileNotFoundError(f"Файл шрифта {font_path} не найден! Укажи правильный путь.")
-    pdfmetrics.registerFont(TTFont("JapaneseFont", font_path))
+    pdfmetrics.registerFont(TTFont(name, font_path))
